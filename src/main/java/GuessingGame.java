f7d76daa3df34dfd80c7cc9149835b7617756462
gooberblox <bwrm2009@gmail.com>
2025-10-20 13:35:34 -0400

    hello fortnite

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessingGame {
@@ -7,5 +8,18 @@ public class GuessingGame {
         // TODO: Use a do...while loop to read guesses until correct
         // TODO: Print "Too low" / "Too high" / "You got it!" accordingly
         // TODO: Use Scanner for input
+        int rand = new Random().nextInt(100);
+        int guess;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Welcome to the Guessing Game!\nI'm thinking of a number between 1 and 100.\n");
+        do {
+            System.out.print("\nEnter your guess: ");
+            guess = scan.nextInt();
+
+            if(guess > rand) System.out.println("Too high! Try again.");
+            if(guess < rand) System.out.println("Too low! Try again.");
+
+        } while(guess != rand);
+        System.out.println("You got it!");
     }
 }
--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -7,5 +7,14 @@ public class ShoppingCart {
         // TODO: Accumulate the total cost
         // TODO: Print the final summary line with item count and total
         // TODO: Use Scanner for input
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many items are you purchasing? ");
+        int amount = scan.nextInt();
+        double sum = 0;
+        for (int i = 1; i <= amount; i++){
+            System.out.print("Price of item "+i+"? ");
+            sum+= scan.nextDouble();
+        }
+        System.out.println("Your cart has "+amount+" items with a total cost of $"+sum);
     }
 }
